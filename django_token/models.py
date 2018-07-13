0c27521abdfab59eed4c24e78b2fec98c3a32898
Martin Gaitan <mgaitan@onapsis.com>
2018-07-13 17:58:45 -0300

    add django2 compat

--- django_token/models.py
+++ django_token/models.py
@@ -1,17 +1,18 @@
-from django.db import models
 import os
-
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 from django.conf import settings
 
 import binascii
 
 
+@python_2_unicode_compatible
 class Token(models.Model):
     """
     An access token that is associated with a user.  This is essentially the same as the token model from Django REST Framework
     """
     key = models.CharField(max_length=40, primary_key=True)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="token")
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name="token", on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
@@ -22,5 +23,5 @@ class Token(models.Model):
     def generate_key(self):
         return binascii.hexlify(os.urandom(20)).decode()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.key
